afa2ab825b981da8eb55344e66dfe1a7e161c6b8
34308 <arcade@spoko.pl>
2022-03-13 14:25:45 +0100

    Dodanie podstawowych informacji: -dodawanie statku -drukowanie tablicy

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Board board=new Board();
+        board.showPlayerBoard();
+        board.placeShips();
+        board.showPlayerBoard();
     }
 }
---
+++ src/com/company/Ship.java
@@ -0,0 +1,54 @@
+package com.company;
+
+public class Ship {
+    //stan statku zbity czy nie
+    //ilośc punktow zycia danego statku
+    int hitPoints;
+    //wielkosc danego statku
+    int size;
+    //pozycje do ktorych jest przypisany na mapie
+    int numberOfPositions;
+    int[][] position;
+    //konstruktor podstawowy statku
+    public Ship() {
+        this.hitPoints = 0;
+        this.size = 0;
+        this.position = new int[1][1];
+    }
+    //konstruktor 2
+    public Ship(int hitPoints, int size, int[][] position) {
+        this.hitPoints = hitPoints;
+        this.size = size;
+        this.position = position;
+    }
+    //umozliwia przypisanie statkowi rozmiaru punktow zycia i pozycji
+    public void changeShip(int size,int[][] position){
+        this.hitPoints = size;
+        this.size = size;
+        this.position = position;
+    }
+    //zwraca pozycje
+    public int[][] getPosition() {
+        return position;
+    }
+    //zwraca rozmiar
+    public int getSize() {
+        return size;
+    }
+    //zmienjsza punkty zycia statku o jeden za kazde wywolanie
+
+    public void hitted(){
+
+        this.hitPoints--;
+        if(hitPoints<=0){
+
+        }
+    }
+    //zwraca alive uzywany do sprawdzenia czy statek nadal funkcjonujes
+    public boolean isAlive(){
+        if(hitPoints<=0){
+            return false;
+        }
+        return true;
+    }
+}
