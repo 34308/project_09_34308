11d93afc7767ecfef8ffb0ab9876f7cebe0335e8
34308 <arcade@spoko.pl>
2022-05-09 12:40:19 +0200

    Mven errors resolve

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,9 @@
 module com.example.battleshipgui {
-    requires javafx.controls;
+    requires javafx.base;
     requires javafx.fxml;
-    requires javafx.web;
+    requires javafx.graphics;
+    requires javafx.controls;
 
-    requires org.controlsfx.controls;
-    requires validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires eu.hansolo.tilesfx;
 
     opens com.example.battleshipgui to javafx.fxml;
     exports com.example.battleshipgui;
